c496889eeab6650ee319f337e8f0a19c70a2953d
rinckarmg <rinckoar@gmail.com>
2021-11-10 23:56:57 -0500

    form

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,12 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Product Screen'),
+      //appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: Icon(Icons.arrow_back_ios_new,
+                          size: 40, color: Colors.white)),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                      onPressed: () {
+                        //TODO: Camara o galeria
+                      },
+                      icon: Icon(Icons.camera_alt_outlined,
+                          size: 40, color: Colors.white)),
+                ),
+              ],
+            ),
+            _ProductForm(),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.save_outlined),
       ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        height: 260,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+            child: Column(
+          children: [
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre del producto', labelText: 'Nombre'),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150', labelText: 'Precio'),
+            ),
+            SizedBox(height: 20),
+            SwitchListTile.adaptive(
+                value: true,
+                title: Text('Disponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value) {
+                  //TODO: validacion
+                }),
+          ],
+        )),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                offset: Offset(0, 5),
+                blurRadius: 5)
+          ]);
 }
